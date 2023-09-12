c3bf0ecd9d675e31215f671643746212deb150f5
tiaradwim1306 <tiaradwim1306@gmail.com>
2023-09-12 15:54:11 +0700

    tes

---
+++ src/2/variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#variable=value
+name="Tiara"
+class=12
+money=17.500
+is=true
+
+#print variable
+echo -e "Hai $name\n"
+echo "now is $(date)"
+echo "you're in class $class"
+echo "you're money is $money"
+echo "and it's $is"
